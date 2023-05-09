665b3f5fa89c435c86a2baac8cf60e47313c9b88
Dineth Rajapakse <dineth.r1@outlook.com>
2023-05-09 17:30:17 +1000

    All models set up

--- db/migrate/20230509061407_create_flight_user.rb
+++
@@ -1,8 +0,0 @@
-class CreateFlightUser < ActiveRecord::Migration[5.2]
-  def change
-    create_table :flight_users do |t|
-      t.integer :user_id
-      t.integer :flight_id
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_09_061407) do
+ActiveRecord::Schema.define(version: 2023_05_09_071416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "flight_users", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "flight_id"
-  end
 
   create_table "flights", force: :cascade do |t|
     t.integer "user_id"
@@ -35,6 +30,14 @@ ActiveRecord::Schema.define(version: 2023_05_09_061407) do
     t.text "plane_name"
     t.integer "rows"
     t.text "columns"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "flight_id"
+    t.text "seat_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
